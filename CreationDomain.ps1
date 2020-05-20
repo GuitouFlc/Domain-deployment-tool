9b3bff2ed824b20dba8e30dcca65799779445b47
Bob Mes Couilles <bob@mescouilles.com>
2020-05-21 00:23:03 +0200

    added DHCP config as a separate file

--- CreationDomain.ps1
+++ CreationDomain.ps1
@@ -1,9 +1,32 @@
-$dnsdomain = "paul.loc"
-$netbiosdomain = "PAUL"
-$recoverypassword = "Password95"
+## Import de la configuration
 
-$safepassword = ConvertTo-SecureString -String $recoverypassword -AsPlainText -Force
+$config = Import-PowerShellDataFile -Path "config.psd1"
+$safepassword = ConvertTo-SecureString -String $config.recoverypassword -AsPlainText -Force
 
-Add-WindowsFeature AD-Domain-Services
+## Active directory
+Add-WindowsFeature -Name "AD-Domain-Services" -IncludeManagementTools
 
-Install-ADDSForest -DomainName $dnsdomain -DomainNetbiosName $netbiosdomain -InstallDns -SafeModeAdministratorPassword $safepassword -Confirm -Force
+try {
+    Install-ADDSForest -DomainName $config.dnsdomain -DomainNetbiosName $config.netbiosdomain -InstallDns -SafeModeAdministratorPassword safepassword -Confirm -Force
+}
+catch [TestFailedException] {
+    Write-Host $_.Exception
+    Write-Host "Error occured"
+}
+
+## DHCP
+Add-WindowsFeature -Name "DHCP" -IncludeManagementTools
+
+## Creating DHCP zones
+foreach ($dhcpconfig in $config.dhcpconfigs) {
+    try {
+        Add-DhcpServerv4Scope -Name ($dhcpconfig.domain + '.' + $config.dnsdomain) -StartRange $dhcpconfig.startrange -EndRange $dhcpconfig.endrange -SubnetMask $dhcpconfig.subnet
+    }
+    catch [Microsoft.Management.Infrastructure.CimException] {
+        Write-Host $_
+    }
+    finally {
+        Write-Host ("Updating " + $dhcpconfig.scopid + " zone")
+        Set-DhcpServerv4Scope -ScopeId $dhcpconfig.scopid -Name ($dhcpconfig.domain + '.' + $config.dnsdomain) -StartRange $dhcpconfig.startrange -EndRange $dhcpconfig.endrange
+    }
+}
---
+++ config.psd1
@@ -0,0 +1,28 @@
+@{
+    dnsdomain = "paul.loc"
+    netbiosdomain = "PAUL"
+    recoverypassword = "Password95"
+    dhcpconfigs = @(
+        @{
+            domain = "granville"
+            scopid = "10.10.10.0"
+            startrange = "10.10.10.1"
+            endrange = "10.10.10.254"
+            subnet = "255.255.255.0"
+        },
+        @{
+            domain = "paris"
+            scopid = "10.10.20.0"
+            startrange = "10.10.20.1"
+            endrange = "10.10.20.254"
+            subnet = "255.255.255.0"
+        },
+        @{
+            domain = "rennes"
+            scopid = "10.10.30.0"
+            startrange = "10.10.30.1"
+            endrange = "10.10.30.254"
+            subnet = "255.255.255.0"
+        }
+    )
+}
